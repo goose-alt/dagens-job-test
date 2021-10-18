9068a8f5ebb8320544031b1c9e3843c8d7c66bfa
albe2669 <albert@risenielsen.dk>
2021-10-18 23:38:06 +0200

    (1) MVP server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors')
 const products = require('./db');
+const uuidV4 = require('uuid').v4;
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,8 +15,26 @@ http.createServer(app).listen(3001, () => {
 });
 
 app.get('/', (_, res) => {
-  res.send({ status: 200 });
+  res.status(200).send();
 });
+
+app.post('/products', (req, res) => {
+  let body = req.body;
+
+  let { name, category, price } = body;
+  let uuid = uuidV4();
+  
+  let product = {
+    id: uuid,
+    name,
+    category,
+    price,
+  };
+
+  products.push(product);
+
+  res.status(200).send({ product });
+})
 
 process.on('SIGINT', function () {
   process.exit();
