ce5e6085235f6540c8421c0fc8ce60d1c3d765cd
albe2669 <albert@risenielsen.dk>
2021-10-19 00:27:27 +0200

    (2) MVP products endpoint

--- server/server.js
+++ server/server.js
@@ -18,6 +18,68 @@ app.get('/', (_, res) => {
   res.status(200).send();
 });
 
+// TODO: Extract the following functions
+app.get('/products', (req, res) => {
+  const { query } = req;
+
+  let page = parseInt(query.page || 1);
+  let prPage = parseInt(query.prPage || 24);
+
+  // Get products, starting at 0, thereby page - 1
+  let filter = (x) => filterCategory(x, query.category) && filterMinPrice(x, parseInt(query.minPrice)) && filterMaxPrice(x, parseInt(query.maxPrice));
+  let filtered = products.filter(x => filter(x));
+
+  let prods = filtered.slice((page - 1) * prPage, page * prPage);
+
+  const nextPage = page + 1;
+  const lastPage = Math.ceil(products.length / prPage);
+
+  res.status(200).send({
+    products: prods,
+    pages: {
+      prPage,
+      page,
+      nextPage,
+      nextPageUrl: req.url.replace(`page=${page}`, `page=${nextPage}`), // Includes other params
+      lastPage,
+      lastPageUrl: req.url.replace(`page=${page}`, `page=${lastPage}`), // Includes other params
+    }
+  })
+});
+
+function filterCategory(product, category) {
+  // Not supplied
+  if (category === undefined) return true;
+  
+  // Filter
+  if (product.category === category) return true;
+
+  // Default
+  return false;
+}
+
+function filterMinPrice(product, minPrice) {
+  // Not supplied
+  if (isNaN(minPrice) || minPrice == undefined) return true;
+  
+  // Filter
+  if (product.price >= minPrice) return true;
+
+  // Default
+  return false
+}
+
+function filterMaxPrice(product, maxPrice) {
+  // Not supplied
+  if (isNaN(maxPrice) || maxPrice === undefined) return true;
+  
+  // Filter
+  if (product.price <= maxPrice) return true;
+
+  // Default
+  return false
+}
+
 app.post('/products', (req, res) => {
   let body = req.body;
 
@@ -34,7 +96,7 @@ app.post('/products', (req, res) => {
   products.push(product);
 
   res.status(200).send({ product });
-})
+});
 
 process.on('SIGINT', function () {
   process.exit();
